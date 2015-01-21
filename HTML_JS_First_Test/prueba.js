a5f467e7fd180cb123ece4f76f3d364c2ec10030
ismael slimane zubillaga <islimane@zeta29.aulas.gsyc.es>
2015-01-21 10:43:44 +0100

    Displaying all elements

--- HTML_JS_First_Test/prueba.js
+++ HTML_JS_First_Test/prueba.js
@@ -1,7 +1,15 @@
-movie={title:'The Godfather',
-	releaseInfo:{'year':1972, rating:'PG'}
+movie = {title:'The Godfather',
+		releaseInfo:{'year':1972, rating:'PG'}
 }
 
 for (i in movie){
-	console.log(movie.i);
+	console.log(i);
+	console.log(movie[i]);
+	// i represents the string value of the element ----> 'releaseInfo'
+	// movie[i] is the current object
+	if(typeof movie[i] != 'string'){
+		for (elem in movie[i]){
+			console.log(elem + ": " + movie[i][elem]);
+		}
+	}
 }
