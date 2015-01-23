5fa03969019279c4c75492cf02989c80fe5fe6cc
Ismael <islimane@islimane-VirtualBox.(none)>
2015-01-23 16:55:41 +0100

    Some changes

--- Empty Page/empty.js
+++ Empty Page/empty.js
@@ -1,7 +1,13 @@
 // Executes 'empty' after the raw markup and DOM of your page has been loaded and parsed.
 document.addEventListener( "DOMContentLoaded", empty, false)
 function empty(){
-	p = document.getElementsByTagName('body');
-	console.log('p: ' + p[0]);
-	p[0].innerHTML = '';
+	tags = document.getElementsByTagName('div');
+	for(i in tags){
+		// May be the tag was already deleted
+		if(tags[i] !== undefined && tags[i].innerHTML !== undefined){
+			console.log('tag[' + i + ']: ' + tags[i]);
+			console.log('tag[' + i + '].innerHTML: ' + tags[i].innerHTML);
+			tags[i].innerHTML = '';
+		}
+	}
 }
