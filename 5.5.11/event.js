f2d236d83adacf0770de180fed27232e42cadb4e
Ismael Slimane <isma.bled777@gmail.com>
2015-02-16 20:11:34 +0100

    Deleted some files

--- 5.5.11/event.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener( 'input', function(){
-	var input = document.getElementById('input').value;
-	console.log(input);
-	var written = document.getElementById('written');
-	written.innerHTML = input;
-});
