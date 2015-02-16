7d76a5eb61304c8e972996aa64ec898f6e45e038
Ismael Slimane <isma.bled777@gmail.com>
2015-02-16 20:13:04 +0100

    Deleted some files

--- Exercises/X-Nav-JS-Event/event.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener( 'input', function(){
-	var input = document.getElementById('input').value;
-	var circle = document.getElementById('circle');
-	console.log(input);
-	circle.style.backgroundColor = input;
-});
