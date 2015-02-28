d2f33f6877bb06c27ae608dfd8bc7c2de235710a
Ismael Slimane <isma.bled777@gmail.com>
2015-02-28 13:54:26 +0100

    Finished SPA Calculator

--- SPA_Calculator/SPACalculator.js
+++ SPA_Calculator/SPACalculator.js
@@ -1,11 +1,9 @@
 $(document).ready(function() {
 	var displayContent = "";
 
-	$("button").click(function() {
+	$("button").click(function(e) {
 		var elemClass = $(this).attr('class');
 		var elemContent = $(this).html();
-		/*console.log(elemClass);
-		console.log(elemContent);*/
 		if(elemClass=="display"){
 			displayContent += elemContent;
 			$("input").val(displayContent);
@@ -17,26 +15,48 @@ $(document).ready(function() {
 				displayContent = "";
 				$("input").val(displayContent);
 			}else if(elemContent=="="){
-				console.log("displayContent: '" + displayContent + "'");
-				if(checkNum(displayContent)==null)
-					$("input").val("OK");
-				else
-					$("input").val(checkNum(elemContent));
+				evaluate();
 			}
 		}
 	});
 
+	$("input").keyup(function(e) {
+		var keyCode = e.which;
+		if(keyCode==171||keyCode==13)
+			evaluate();
+		displayContent = $(this).val();
+		var keyPressed = displayContent.substring(displayContent.length-1, displayContent.length);
+		var elemContent = keyPressed;
+		console.log("keyPressed: " + keyPressed);
+		console.log("keyCode: " + keyCode);
+	});
+
+	function evaluate(){
+		var ok = checkNum(displayContent);
+		console.log("displayContent: '" + displayContent + "'");
+		if(displayContent != '')
+			if(ok==null){
+				displayContent = eval(processCalc(displayContent)).toString();
+				$("input").val(displayContent);
+			}else
+				$("input").val(ok);
+	}
+
 	function checkNum(str) {
 		for (var i = 0; i < str.length; i++) {
 			var ch = str.substring(i, i+1);
-			if (ch < "0" || ch > "9") {
-				console.log("IS NOT A NUMBER");
-				if (ch != "/" && ch != "*" && ch != "+" && ch != "-" && ch != "(" && ch!= ")" && ch!= "&divide;") {
-					console.log("IS NOT A VALID CHARACTER");
-					return("invalid entry: '" + ch + "'");
-				}
-			}
+			if (ch < "0" || ch > "9")
+				if (ch != "/" && ch != "*" && ch != "+" && ch != "-" && ch != "(" && ch!= ")" && ch!= "×" && ch!="÷" && ch!="." && ch!="Π" && ch!="x")
+					return ("invalid entry: '" + ch + "'");
 		}
 		return null;
+	}
+
+	function processCalc(calc){
+		calc = calc.replace("×","*");
+		calc = calc.replace("x","*");
+		calc = calc.replace("÷","/");
+		calc = calc.replace("Π","3.141592654");
+		return calc;
 	}
 });
