3759e4e21546aed87fb4f9f9c63c83eb2af54a1a
ismael slimane zubillaga <islimane@lambda14.aulas.gsyc.es>
2015-03-03 11:58:46 +0100

    Simple getJSON() example

--- jQuery/5.6.6/json-data.js
+++ jQuery/5.6.6/json-data.js
@@ -2,15 +2,26 @@
 // Get new text when clicking on "third"
 //
 $(document).ready(function() {
-    $("#third").click(function(){
-	$.getJSON("json-data.json", function(data) {
-	    $('#header').after('<h1>' + data.title + '</h1>');
-	    list = '<ul>'
-	    for (var i = 0; i < data.entries.length; i++) {
-		list = list + '<li>' + data.entries[i] + '</li>';
-	    }
-	    list = list + '</ul>';
-	    $('#second').after(list);
+	$("#btn").click(function(){
+		$.getJSON("json-data.json", function(data) {
+			$('#header').html('<h1>' + data.hi + '</h1>');
+		});
 	});
+
+	//Another way
+	/*$.ajax({
+		type:"GET",
+		dataType: "JSON",
+		url: "json-data.json",
+		cache: false,
+		success: function(data){
+			event(data);
+		}
     });
+
+	var event = function(data){
+		$("#btn").click(function(){
+			$('#header').html('<h1>' + data.hi + '</h1>');
+		});
+	};*/
 });
