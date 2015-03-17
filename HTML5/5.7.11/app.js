93782a3ebc8afaf632212e3e63e686e8002eb56d
ismael slimane zubillaga <islimane@zeta05.aulas.gsyc.es>
2015-03-17 19:23:48 +0100

    HTML5 Exercises

---
+++ HTML5/5.7.11/app.js
@@ -0,0 +1,47 @@
+jQuery(document).ready(function() {
+	var map = L.map('map').setView([40.2838, -3.8215], 13);
+
+	L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+		maxZoom: 18
+	}).addTo(map);
+
+	var marker = L.marker([40.2838, -3.8215]).addTo(map);
+
+	var circle = L.circle([40.2838, -3.8215], 200, {
+		color: 'red',
+		fillColor: '#f03',
+		fillOpacity: 0.5
+	}).addTo(map);
+
+	marker.bindPopup("URJC").openPopup();
+
+
+	// SHOW POSITION
+	var x = document.getElementById("demo");
+
+	function getLocation() {
+		if (navigator.geolocation) {
+		    navigator.geolocation.getCurrentPosition(showPosition);
+		} else {
+		    x.innerHTML = "Geolocation is not supported by this browser.";
+			console.log("NO HAY GEOLOC");
+		}
+	}
+
+	function showPosition(position) {
+		x.innerHTML = "Latitude: " + position.coords.latitude + 
+		"<br>Longitude: " + position.coords.longitude; 
+		console.log("latitude: " + latitude);
+		console.log("longitude: " + longitude);
+		console.log("HOLA: ");
+	}
+
+	if(Modernizr.geolocation){
+		alert("HAY geolocation");
+	}
+
+	console.log("antes de geoloc");
+
+	getLocation();
+
+});
---
+++ HTML5/5.7.9/app.js
@@ -0,0 +1,23 @@
+if(Modernizr.canvas){
+	alert("HAY CANVAS");
+}
+
+if(Modernizr.video){
+	alert("HAY video");
+}
+
+if(Modernizr.video.ogg){
+	alert("HAY video.ogg");
+}
+
+if(Modernizr.localStorage){
+	alert("HAY localStorage");
+}
+
+if(Modernizr.applicationcache){
+	alert("HAY applicationcache");
+}
+
+if(Modernizr.geolocation){
+	alert("HAY geolocation");
+}
